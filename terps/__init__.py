85662bb8a067c25679b78b046ae692395ed9b89c
Manas M Bhat <bhatmanas12@gmail.com>
2023-01-04 23:25:13 -0500

    v2

--- terps/__init__.py
+++ terps/__init__.py
@@ -2,10 +2,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import mysql.connector
 
+#Create a flask instance
 app = Flask(__name__)
+
+#To prevent cookies and cross-site attacks
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitetest.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitetest.db'
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="manasmb",
+    password="manas4321",
+    hostname="manasmb.mysql.pythonanywhere-services.com",
+    databasename="manasmb$sitetest",
+)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 280
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- terps/models.py
+++ terps/models.py
@@ -16,6 +16,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
+#How the object should be printed:
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
